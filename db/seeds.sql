0a25e09e86544b0fd6422667900124d282bc8834
Andrew Dalba <andrewdalba@gmail.com>
2020-08-31 15:10:54 -0400

    change variable names

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (name) VALUES ('Smash Burger');
 INSERT INTO burgers (name) VALUES ('Big Mac');
-INSERT INTO burgers (name, sleepy) VALUES ('Double Double', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Obama Burger', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Baconator', true);
+INSERT INTO burgers (name, eaten) VALUES ('Double Double', true);
+INSERT INTO burgers (name, eaten) VALUES ('Obama Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Baconator', true);
 INSERT INTO burgers (name) VALUES ('Quater Pounder');
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,20 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".change-eaten").on("click", function(event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
+    var newEaten = $(this).data("newEaten");
 
-    var newSleepState = {
-      sleepy: newSleep
+    var newEatenState = {
+      eaten: newEaten
     };
 
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: newEatenState
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        console.log("changed eaten to", newEaten);
         // Reload the page to get the updated list
         location.reload();
       }
@@ -27,7 +27,7 @@ $(function() {
 
     var newburger = {
       name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+      eaten: $("[name=eaten]:checked").val().trim()
     };
 
     // Send the POST request.
