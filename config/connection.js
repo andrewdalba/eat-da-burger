52282f55c0872b8f52b307ff910e7f3f3b5668b3
Andrew Dalba <andrewdalba@gmail.com>
2020-09-01 18:39:17 -0400

    edit connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,34 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+// // Set up MySQL connection.
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Andyboy1!",
-  database: "burger_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Andyboy1!",
+//   database: "burger_db"
+// });
+
+
+
+var mysql = require("mysql");
+var connection = null;
+if (typeof process.env.JAWSDB_URL === "undefined") {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Andyboy1!",
+    database: "burger_db"
+  });
+}
+else {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log(process.env.JAWSDB_URL);
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -18,5 +36,11 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+
+
+
+
 // Export connection for our ORM to use.
 module.exports = connection;
+
+
